e316504b839082b0f79327320d1234cabf9eba57
Strsavka <starslavavlad@gmail.com>
2025-03-18 19:20:59 +0400

    markup_menu and markup_stop

--- homework_tg_bot_test.py
+++ homework_tg_bot_test.py
@@ -1,6 +1,6 @@
 import logging
 import sqlite3
-from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, KeyboardButton
 from telegram.ext import Application, MessageHandler, filters, CommandHandler, ConversationHandler
 
 # TOKEN for Telegram-bot is necessary
@@ -10,9 +10,13 @@ BOT_TOKEN = '7596957066:AAH5kPuJH1823dvaoIbaEYB7T79Y9oP-P78'
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
-reply_keyboard = [['Изменить класс', 'Инструктаж'],
-                  ['Отправить домашнее задание', 'Получить домашнее задание']]
-markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
+# mark_ups making
+keyboard_for_menu = [[KeyboardButton('Получить дз'), KeyboardButton('Отправить дз')],
+                     [KeyboardButton('Инструктаж'), KeyboardButton('Сменить класс')]]
+markup_menu = ReplyKeyboardMarkup(keyboard_for_menu)
+
+stop_button = [['/stop']]
+markup_stop = ReplyKeyboardMarkup(stop_button)
 
 # database connection
 connection = sqlite3.connect("homework_database.sqlite")
@@ -20,7 +24,12 @@ cursor = connection.cursor()
 
 
 async def text_answer(updater, context):
-    await updater.message.reply_text('Запрос не принят')
+    if updater.message.text == 'Сменить класс':
+        await updater.message.reply_text('Чтобы сменить класс напиши цифру класса и букву класса без пробелов',
+                                         reply_markup=markup_stop)
+        return 1
+    else:
+        await updater.message.reply_text('Запрос не принят')
 
 
 async def start(updater, context):
@@ -36,11 +45,11 @@ async def start(updater, context):
             await updater.message.reply_text(f'Привет {first_name}! Я телеграм-бот, работающий на ГБОУ'
                                              f' УР Лицей №41, предназначен для помощи лицеистам получать домашнее задание')
             await updater.message.reply_text(f'Чтобы было проще сразу скажи в каком классе ты учишься, напиши цифру класса '
-                                             f'и букву без пробелов')
+                                             f'и букву без пробелов', reply_markup=markup_stop)
             return 1
         else:
-            await updater.message.reply_text(f'Привет {first_name}, похоже ты у нас уже был, если ты '
-                                             f'забыл как работать со мной то просто вызови инструкцию')
+            await updater.message.reply_text(f'Привет {first_name}, похоже ты у нас уже был, если ты забыл как '
+                                             f'работать со мной то просто вызови инструкцию', reply_markup=markup_menu)
     except Exception as e:
         await error(updater, context, e)
 
@@ -53,7 +62,7 @@ async def class_asking(updater, context):
             cursor.execute('''UPDATE users SET (class, letter_of_class) = (?, ?) WHERE telegram_id = ?''',
                            (user_class[0], user_class[1].lower(), updater.message.chat.id))
             connection.commit()
-            await updater.message.reply_text('Отлично', reply_markup=markup)
+            await updater.message.reply_text('Отлично', reply_markup=markup_menu)
             return ConversationHandler.END
         else:
             await updater.message.reply_text('Некорректный ввод, попробуйте ещё раз, цифру, букву, без пробелов, '
@@ -64,7 +73,7 @@ async def class_asking(updater, context):
 
 
 async def stop(updater, context):
-    await updater.message.reply_text('OK')
+    await updater.message.reply_text('OK', reply_markup=markup_menu)
     return ConversationHandler.END
 
 
@@ -83,8 +92,8 @@ def initialization():
     application = Application.builder().token(BOT_TOKEN).build()
 
     # A handler for asking user about his class
-    entry_class_handler = ConversationHandler(
-        entry_points=[CommandHandler('start', start)],
+    entry_or_change_class_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', start), MessageHandler(filters.TEXT & ~filters.COMMAND, text_answer)],
         states={1: [MessageHandler(filters.TEXT & ~filters.COMMAND, class_asking)]},
         fallbacks=[CommandHandler('stop', stop)]
     )
@@ -93,7 +102,7 @@ def initialization():
     text_handler = MessageHandler(filters.TEXT & ~filters.COMMAND, text_answer)
 
     # Registration of handlers
-    application.add_handler(entry_class_handler)
+    application.add_handler(entry_or_change_class_handler)
     application.add_handler(text_handler)
     application.run_polling()
 
